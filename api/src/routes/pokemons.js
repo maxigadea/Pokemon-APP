be05bca6b5cd61317816d1e4e0ee41daf455d001
Maximiliano Gadea <85596286+maxigadea@users.noreply.github.com>
2021-09-26 20:02:13 -0300

    Routes 2

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -1,11 +1,45 @@
-const { default: axios } = require('axios');
 const {Router} = require('express');
+const  axios  = require('axios');
+const { Pokemon, Types} = require('../db');
 
 const router = Router();
 
-router.get("/", async(req, res) => {
-    const apiUrl = await axios.get("/https://pokeapi.co/api/v2/pokemon?limit=40")
-    const apiInfo = await apiResults.data.map(elemento => {
+router.get("/", async(req, res, next) => {
+    try {
+        //Traigo los de la DB
+        let pokemonesDB = await Pokemon.findAll( {include: Types} )
+
+        if(pokemonesDB) {
+            pokemonesDB.map(p => {
+                return {
+                    id: p.id,
+                    name: p.name,
+                    type: p.dataValues.types.map( p => p.dataValues.name),
+                    createdInDb: p.createdInDb,
+                    attack: p.attack,
+                }
+            })
+        }
+        //Traigo los de la API
+        const apiUrl = await axios.get('https://pokeapi.co/api/v2/pokemon?limit10');
+        const apiInfo = apiUrl.data.results;
+        let pokemonData = [];
+        for(p of apiInfo) {
+            let subRequest = p.url;
+            let subRequestData = await axios.get(`${subRequest}`)
+            pokemonData.push({
+                id:subRequestData.data.id,
+                name: subRequestData.data.name,
+                type: subRequestData.data.types.map(e => e.type.name),
+                image: subRequestData.data.sprites.other.dream_world.front_default,
+                attack: subRequestData.data.stats[1].base_stat,
+            })
+            res.status(200).send(pokemonesDB.concat(pokemonData))
+        } 
+    } catch (error) {
+        next("Api no responde")
+    }
+    
        
 });
 
