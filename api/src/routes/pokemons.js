7ae9727a93c348ffab2f1e8fb07555a7056f868b
Maximiliano Gadea <85596286+maxigadea@users.noreply.github.com>
2021-10-03 12:04:59 -0300

    BackEnd Done

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -2,6 +2,8 @@ const express = require('express')
 const {Router} = require('express');
 const  axios  = require('axios');
 const { Pokemon, Types} = require('../db');
+const { v4: uuidv4 } = require('uuid');
+const types = require('../models/types');
 
 
 
@@ -90,6 +92,73 @@ router.get("/", async(req, res, next) => {
     } catch (error) {
         next("Api not found")
     }    
+});
+
+router.get('/:id', async(req, res, next) => {
+    const {id} = req.params;
+    if(id.length > 4 ) {
+        try {
+            const pokemonDB = await Pokemon.findOne({where: {id:id}, include: Types});
+            const { name, hp, attack, defense, speed, weight, height, image} = pokemonDB;
+            const response = {
+                name,
+                hp,
+                attack,
+                defense,
+                speed,
+                weight,
+                height,
+                image,
+                type: pokemonDB.data.types.map( p => p.name)
+            }
+            return res.json(response);
+        } catch (error) {
+            next('ID not found')
+        }
+    } else {
+        try {
+            const pokemonApi = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            let response = {
+                id: pokemonApi.data.id,
+                name: pokemonApi.data.name,
+                type: pokemonApi.data.types.map(e => e.type.name),
+                image: pokemonApi.data.sprites.other.dream_world.front_default,
+                attack: pokemonApi.data.stats[1].base_stat,
+                hp: pokemonApi.data.stats[0].base_stat,
+                defense: pokemonApi.data.stats[2].base_stat,
+                speed: pokemonApi.data.stats[5].base_stat,
+                weight: pokemonApi.data.weight,
+                height: pokemonApi.data.height,
+            }
+            return res.json(response);
+        } catch (error) {
+            next('ID Not Found')
+        }
+    }
+});
+
+router.post('/', async(req, res, next) => {
+    const {name, hp, attack, defense, speed, weight, height, type, image} = req.body;
+    try {
+        const newPokemon = await Pokemon.create({
+            id: uuidv4(),
+            name,
+            hp,
+            attack,
+            defense,
+            speed,
+            weight,
+            height,
+            image
+        })
+         let typesDB = await Types.findAll({
+             where: {name: type}
+         })
+         newPokemon.addTypes(typesDB)
+         res.send(newPokemon)
+    } catch (error) {
+        next(error)
+    }
 });
 
 module.exports = router;
--- api/src/routes/types.js
+++ api/src/routes/types.js
@@ -1,10 +1,19 @@
 const {Router} = require('express');
 const axios = require('axios');
+const { Pokemon, Types} = require('../db');
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("funciona 1")
+router.get("/", async (req, res) => {
+    const typesApi = await axios.get('https://pokeapi.co/api/v2/type')
+    let result = typesApi.data.results;
+    const typesNames = result.map(el => el.name)
+    typesNames?.map(e => { Types.findOrCreate({
+        where: { name: e} 
+    }) 
+    })  
+    const alltypes = await Types.findAll()
+    res.send(alltypes)
 });
 
 module.exports = router;
